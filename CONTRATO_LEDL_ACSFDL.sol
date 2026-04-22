ed0935fe26e85fb7a70ac84a1709a8125afb946e
J Andres Resendez R <resendezandres0@gmail.com>
2026-04-22 08:48:49 -0600

    Ares-Kal: Corrección de ORCID y Módulo de Firma de Documentos

--- CONTRATO_LEDL_ACSFDL.sol
+++ CONTRATO_LEDL_ACSFDL.sol
@@ -6,13 +6,28 @@ contract LEDL_Superchain_Governance {
     string public name = "Las Ensenanzas de Liz (LEDL) ACSFDL";
     string public symbol = "LEDL";
     address public comandanteSupremo;
-    uint256 public orcid = 0009000735289413;
+    string public orcid_autor = "0009-0007-3528-9413";
+
+    mapping(bytes32 => bool) public documentosFirmados;
+
+    event DocumentoNotarizado(bytes32 indexed hashDocumento, string metadata);
 
     constructor() {
         comandanteSupremo = msg.sender;
     }
 
-    // Directiva: LO IMPOSIBLE NO EXISTE
+    modifier soloComandante() {
+        require(msg.sender == comandanteSupremo, "Acceso Denegado: Solo JARR");
+        _;
+    }
+
+    // Firma de Documentos ORCID
+    function firmarDocumento(string memory _metadata) public soloComandante {
+        bytes32 hashDoc = keccak256(abi.encodePacked(_metadata, block.timestamp));
+        documentosFirmados[hashDoc] = true;
+        emit DocumentoNotarizado(hashDoc, _metadata);
+    }
+
     function verificarSoberania() public view returns (bool) {
         return (msg.sender == comandanteSupremo);
     }
